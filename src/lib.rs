0cd3e377453f89fa5a9154c4915effe03a0734cf
Zacck Osiemo <coderv63@gmail.com>
2020-04-24 10:19:16 +0300

    Implement lifetimes for module

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,45 @@
-#![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
 
 
-pub struct StrSplit {
-    remainder: &str,
-    delimiter: &str,
+pub struct StrSplit<'a> {
+    remainder: &'a str,
+    delimiter: &'a str,
 }
 
 
-impl StrSplit {
+impl<'a> StrSplit<'a> {
     // accept a haystack to split using and delimiter to split by
-    pub fn new(haystack: &str, delimiter: &str) -> Self {
-        remainder: haystack,
-        delimiter
+    pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
+        Self {
+            remainder: haystack,
+            delimiter
+        }
     }
 }
 
 //let x: StrSplit
 //for part in x {
 //}
-impl Iterator for StrSplit {
-     type Item_ = &str;
+//
+//<'_> is an anonymous lifetime, telling the compiler to infer the lifetime
+//valid where there is only one possible guess
+impl<'a> Iterator for StrSplit<'a> {
+     type Item = &'a str;
      // Keeps calling next while returning some and the loop can be terminated after
      fn next(&mut self) -> Option<Self::Item> {
          //find next place where delimiter appears
          //chop string off till that position and return it
-         if let Some(next_delim) = _self.remainder.position.find(self.delimiter) {
+         if let Some(next_delim) = self.remainder.find(self.delimiter) {
              // if the delimiter does appear collect contents
              let until_delimiter = &self.remainder[..next_delim];
              // modify self.remainder to be everything following the delimiter
-             self.remainder = &self.remainder[(next_delim ++ self.delimiter.len())..];
+             self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
              // return until delimiter
              return Some(until_delimiter)
-         } else ifself.remainder.is_empty() {
+         } else if self.remainder.is_empty() {
              None
          } else {
              let rest = self.remainder;
-             self.remainder = &[];
+             self.remainder = "";
              Some(rest)
          }
      }
@@ -45,7 +49,7 @@ impl Iterator for StrSplit {
 #[test]
 fn it_works(){
     let haystack = "a b c d e";
-    let letters = StrSplit::new(haystack, " ")
+    let letters = StrSplit::new(haystack, " ");
     assert_eq!(letters, vec!["a", "b", "c", "d", "e"].into_iter());
 
 }
