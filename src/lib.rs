28f35f7812d9a0b0ae2f837b1b62a73d0f5f4079
Zacck Osiemo <coderv63@gmail.com>
2020-04-26 10:15:28 +0300

    A bunch of work to compile - Add tail test to catch an empty tail - Clean up next to compile - Correct Remainder type
    * follow the positions of the collection in next and you should be fine

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 
 #[derive(Debug)]
 pub struct StrSplit<'a> {
-    remainder: &'a str,
+    remainder: Option<&'a str>,
     delimiter: &'a str,
 }
 
@@ -10,7 +10,7 @@ impl<'a> StrSplit<'a> {
     // accept a haystack to split using and delimiter to split by
     pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
         Self {
-            remainder: haystack,
+            remainder: Some(haystack),
             delimiter
         }
     }
@@ -26,24 +26,25 @@ impl<'a> Iterator for StrSplit<'a> {
      type Item = &'a str;
      // Keeps calling next while returning some and the loop can be terminated after
      fn next(&mut self) -> Option<Self::Item> {
-         //find next place where delimiter appears
-         //chop string off till that position and return it
-         if let Some(next_delim) = self.remainder.find(self.delimiter) {
-             // if the delimiter does appear collect contents
-             let until_delimiter = &self.remainder[..next_delim];
-             // modify self.remainder to be everything following the delimiter
-             self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
-             // return until delimiter
-             return Some(until_delimiter)
-         } else if self.remainder.is_empty() {
-             None
-         } else {
-             let rest = self.remainder;
-             self.remainder = "";
-             Some(rest)
-         }
+        if let Some(ref mut remainder) = self.remainder {
+            //find next place where delimiter appears
+            //chop string off till that position and return it
+            if let Some(next_delim) = remainder.find(self.delimiter) {
+                // if the delimiter does appear collect contents
+                let until_delimiter = &remainder[..next_delim];
+                // modify self.remainder to be everything following the delimiter
+                *remainder = &remainder[(next_delim + self.delimiter.len())..];
+                // return until delimiter
+                return Some(until_delimiter)
+            } else {
+                self.remainder.take()
+            }
+        } else {
+            None
+        }
      }
 }
+
 
 
 #[test]
@@ -51,4 +52,11 @@ fn it_works(){
     let haystack = "a b c d e";
     let letters = StrSplit::new(haystack, " ");
     assert!(letters.eq(vec!["a", "b", "c", "d", "e"].into_iter()));
+}
+
+#[test]
+fn tail() {
+  let haystack = "a b c d ";
+  let letters: Vec<_> = StrSplit::new(haystack, " ").collect();
+  assert_eq!(letters, vec!["a", "b", "c", "d", ""]);
 }
